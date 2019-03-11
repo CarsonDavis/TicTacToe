5a842d9af796bff5732a83861dda311fe1e0ad8a
CarsonReidDavis <CarsonReidDavis@gmail.com>
2019-03-10 21:10:10 -0500

    major refactor
    combined the various play functions. added a new function to handle user
    input and added input validation. removed optional arguments and made are
    argument passing consistent throughout. added optional printing functionality
    to play function. cleaned code in general.

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,7 +3,8 @@ from keras.models import load_model
 import numpy as np
 model = load_model('Models and Data/First Attempt Refactor/both_players_model.h5')
 
-
+# TODO: think about tracking the current player instead of passing around piece to every function
+# TODO: integrate the play function into the class
 class Game:
     def __init__(self, player_piece=1):
         self.current_board = [[0, 0, 0],
@@ -18,28 +19,23 @@ class Game:
         self.game_over = False
         self.winner = None
 
-    def move(self, position, piece=None):
-        # position is int 0-8
-        # piece is {'O':-1, 'X':1}
-        # will accept invalid moves, error handling needs to be somewhere else
-
+    def move(self, position, piece):
+      
+        # update board and move history
         # so this ridiculous bit is necessary because otherwise python will use references on the internal lists
         self.board_history += [[[j for j in i] for i in self.current_board]]
         self.move_history.append(position)
 
-        if not piece:
-            piece = self.player_piece
-
-        position = int(position)
-
+        # remove move from total possible moves
         self.valid_moves.remove(position)
+
+        # make the move by updating the current board
         self.current_board[position // 3][position % 3] = piece
-        self.game_over = self.is_victory()
+        
+        # update the game over variable
+        self.game_over = self.is_victory(self.current_board)
 
-    def is_victory(self, board=None):
-
-        if not board:
-            board = self.current_board
+    def is_victory(self, board):
 
         # rows
         for row in board:
@@ -65,83 +61,63 @@ class Game:
 
         return False
 
-    def print_board(self, board=None):
-        if not board:
-            board = self.current_board
-
+    def print_board(self, board):
         for row in range(3):
             print(board[row])
 
-    def board_to_prediction_list(self, board=None):
-        if not board:
-            board = self.current_board
+    def board_to_vector(self, board, piece):
 
+        # reverse board if player 2
+        if piece == -1:
+            board = [[num * -1 for num in row] for row in board]
+
+        # turn board into vector consumable by model
         vector_board = []
         for row in board:
             vector_board += row
         np_board = np.array([vector_board])
 
-        return list(model.predict(np_board)[0])
+        return np_board
 
-    def random_move(self, piece=None):
-
-        if not piece:
-            piece = self.computer_piece
-
-        if not self.valid_moves:
-            print('aaaahhhh')
-
+    def random_move(self, piece):
         return choice(list(self.valid_moves)), piece
 
-    def smart_move(self, piece=None):
+    def smart_move(self, piece):
+        
+        # turn the board into a vector that can be consumed by the model
+        board_vector = self.board_to_vector(self.current_board, piece)
 
-        if not piece:
-            piece = self.computer_piece
-
-        print(piece)
-
-        prediction_list = self.board_to_prediction_list()
+        # run the board through the model and generated predictions
+        prediction_list = list(model.predict(board_vector)[0])
 
         while True:
+            # take the move with the highest predicted value then remove it from the prediction list
             move = prediction_list.index(max(prediction_list))
             prediction_list[move] = 0
-
+            
+            # make sure the move predicted by the model is actually available
             if move in self.valid_moves:
                 return move, piece
+    
+    def player_move(self, piece):
+        
+        try:
+            move = int(input())
+        except ValueError:
+            move = None
 
-def play():
+        while move not in self.valid_moves:
+            print('Please choose a valid move from', self.valid_moves)
+            move = int(input())
+
+        return move, piece
+
+def play(print_on=True):
+    # change the functions in the turn map if you want to modify who plays who
     game = Game()
 
-    turn_map = {-1: {'name': 'computer', 'move function': game.random_move},
-                1: {'name': 'player', 'move function': input}
-                }
-
-    whose_move = 1
-
-    print('Your Move')
-    game.print_board()
-
-    while not game.game_over:
-
-        if not game.valid_moves:
-            print('there was a tie')
-            break
-
-        game.move(*turn_map[whose_move]['move function']())
-
-        print(turn_map[whose_move]['name'] + "'s move")
-        game.print_board()
-        print()
-        whose_move *= -1
-
-    print(turn_map[(whose_move * -1)]['name'], ' wins!')
-
-def make_data():
-    game = Game()
-
-    turn_map = {-1: {'name': 'player 2', 'move function': game.random_move},
-                1: {'name': 'player 1', 'move function': game.smart_move},
-                'tie': {'name': 'nobody'}
+    turn_map = {1: {'name': 'Player 1', 'move function': game.smart_move},
+               -1: {'name': 'Player 2', 'move function': game.player_move},
                 }
 
     whose_move = 1
@@ -151,15 +127,27 @@ def make_data():
         if not game.valid_moves:
             game.winner = 'tie'
             break
+        
+        if print_on:
+            print(turn_map[whose_move]['name'] + "'s Move")
+            game.print_board(game.current_board)
+            print() 
 
         game.move(*turn_map[whose_move]['move function'](whose_move))
         whose_move *= -1
 
-    if not game.winner:
-        game.winner = whose_move * -1
-
     if game.winner != 'tie':
-        return game.board_history, game.move_history, game.winner
+        game.winner = whose_move * -1    
     else:
-        return None, None, game.winner
+        game.board_history = None
+        game.move_history = None
+    
+    if print_on:
+        game.print_board(game.current_board)
+        print()
+        if game.winner == 'tie':
+            print('There was a tie!')
+        else:
+            print(turn_map[(whose_move * -1)]['name'], ' wins!')       
 
+    return game.board_history, game.move_history, game.winner
