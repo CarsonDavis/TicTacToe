f8cb9c067f95f9d29001b088a3d4116ac94c2c18
CarsonReidDavis <CarsonReidDavis@gmail.com>
2019-03-18 22:49:42 -0500

    revamp probability handling and console logging

--- deep_learning.py
+++ deep_learning.py
@@ -5,22 +5,23 @@ from keras.utils.np_utils import to_categorical
 from keras import models, layers, optimizers, losses, metrics
 from make_data import make_data
 
-winning_boards, model_test = make_data(model = None, iterations = 1000, probability=0)
-probability = .5
+# generate baseline data for the first training of the model
+winning_boards, winner_history = make_data(model = None, iterations = 1000, probability=0)
 
 
 print()
 print('##############################################################################################################################################')
-print('results of the initial random games')
-print(model_test)
-print('wins using random', model_test[1]['wins'])
+print('Winners of the intial random games')
+print(f'Player 1: {winner_history[1]["wins"]/10} %')
+print(f'Player 2: {winner_history[-1]["wins"]/10} %')
+print(f'Tie Game: {winner_history["tie"]["wins"]/10} %')
 print('##############################################################################################################################################')
 print()
 
 all_winning_boards = winning_boards['boards']
 all_winning_moves = winning_boards['moves']
 
-for model_iteration in range(10):
+for model_iteration in range(50):
 
     X = np.array(all_winning_boards)
     y = to_categorical(all_winning_moves)
@@ -51,16 +52,37 @@ for model_iteration in range(10):
                     batch_size = 512,
                     validation_data = (X_test, y_test))
 
+    print()
+    print('##############################################################################################################################################')
+    print('Evaluation new model on the validation data')
     print(model.evaluate(X_val, y_val))
+    print()
 
+    # run the new model to see how well it does when played 100% of the time
+    ignore_me, winner_history = make_data(model = model, iterations = 1000, probability=1)
     
-    print('probability to use with new model', probability)
-    winning_boards, winner_history = make_data(model = model, iterations = 1000, probability=probability)
-    ignore_me, model_test = make_data(model = model, iterations = 1000, probability=1)
-    print('wins with new model', model_test[1]['wins'])
+    # probability of player 1 using the model in the next training set is calculated by the percent games won or tied
+    probability = (1000-winner_history[-1]['wins'])/1000
+    # print(winner_history)
+    # print('percent wins with new model', winner_history[1]['wins'])
+    # print('probability to use new model in next data collection', probability)
+
+
+    print('Win percentages with the new model')
+    print(f'Player 1: {winner_history[1]["wins"]/10} %')
+    print(f'Player 2: {winner_history[-1]["wins"]/10} %')
+    print(f'Tie Game: {winner_history["tie"]["wins"]/10} %')
+    print()
+    print(f'Probabilty of using new model in the next data collection {probability}')
     print('##############################################################################################################################################')
     print()
 
-    probability = (model_test[1]['wins']/1000)
+    # use the new model to collect new data. also use random moves to as a proxy for creativity
+    winning_boards, winner_history = make_data(model = model, iterations = 1000, probability=probability)
+
+    # add newly generated training data to the corpus
     all_winning_boards += winning_boards['boards'].copy()
     all_winning_moves += winning_boards['moves'].copy()
+
+
+
